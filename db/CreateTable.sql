49705875d9842685ae87ed1bd42aa92c9def9d2b
HarrisonSteer1 <117164747+HarrisonSteer1@users.noreply.github.com>
2023-03-17 12:51:54 +0000

    Update CreateTable.sql

--- db/CreateTable.sql
+++ db/CreateTable.sql
@@ -14,3 +14,4 @@ LOCK TABLES `users` WRITE;
 INSERT INTO `users` VALUES (1,'Ben','Hesketh','test@test7.com'),(2,'Luke','Benson','test@test.com'),(3,'Matt','Hunt','test4@test.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
